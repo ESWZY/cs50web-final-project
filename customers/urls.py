a930f6bd283f8df447a61f1e9c0c8eca2d1bbb8c
ESWZY <1006460505@qq.com>
2020-04-19 21:38:36 +0800

    Login function

--- customers/urls.py
+++ customers/urls.py
@@ -5,6 +5,6 @@ app_name = "customers"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    # path("login", views.login_view, name="login"),
-    # path("logout", views.logout_view, name="logout")
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,31 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    if not request.user.is_authenticated:
+        print(1234567890)
+        return render(request, "customers/login.html", {"message": None})
+    context = {
+        "user": request.user
+    }
+    return render(request, "customers/user.html", context)
+
+
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("customers:index"))
+    else:
+        return render(request, "customers/login.html", {"message": "Invalid credentials."})
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, "customers/login.html", {"message": "Logged out."})
