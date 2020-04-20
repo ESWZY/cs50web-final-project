c8feffbc3cef0b9f150333abf8353c5fcdf73b87
ESWZY <1006460505@qq.com>
2020-04-20 22:04:07 +0800

    Customer OnetoOne User

--- customers/models.py
+++ customers/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Customer(models.Model):
-    name = models.CharField(max_length=64, verbose_name="Name")
+    name = models.CharField(max_length=64, verbose_name="Name", default='SOME STRING')
     sub = models.BooleanField(verbose_name="Is subscriber", default=0)
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name}: {self.sub}"
